72a31ab80fe5633b010fe02d92f2f7527fa1d997
isham-26 <ishamgupta711@gmail.com>
2024-06-10 01:40:33 +0530

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="bg-pink-50/20 h-[2rem] mt-10 flex justify-center items-center italic">
+    <div className="bg-pink-50/20 h-[2rem] mt-10 flex justify-center items-center italic xl:sticky xl:bottom-0 w-full">
         &#169; This Portfolio Created By-<div className="text-accent font-semibold"> Isham Gupta</div> 
     </div>
   )
