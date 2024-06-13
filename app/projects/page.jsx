aaf7d29500f8bd0f191622c0d609347df43bdd05
isham-26 <ishamgupta711@gmail.com>
2024-06-13 12:40:46 +0530

    all done

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,32 +1,89 @@
 "use client";
 import Card from "@/components/Card";
-import Image from "next/image";
+import { FaNodeJs, FaReact, FaInstagram } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaLinkedinIn, FaGithub} from "react-icons/fa";
+import {  SiLeetcode, SiCodeforces,SiPostman,SiCodechef } from "react-icons/si";
+import {
+  SiExpress,
+  SiFastapi,
+  SiMongodb,
+  SiMysql,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import { TbBrandCpp, TbBrandNextjs } from "react-icons/tb";
+import { IoLogoFirebase } from "react-icons/io5";
 const Projects = () => {
   const data =[
     {
       num:1,
-      title:"mark website",
-      categiry:"full stack",
+      title:"Mark Website",
+      categiry:"Full Stack",
       img:"project.jpg",
-      discription:"lorem bnqwsd usd qwsuw sws swsqw sws sisqws i sqwghws wsus sw suiz shqwsuq sqwuishs qws qwhs",
+      tools:[
+        <FaReact key={9} />,
+        <FaNodeJs key={8} />,
+        <SiExpress key={7} />,
+        <TbBrandNextjs key={6} />,
+        <SiTailwindcss key={5} />,],
+      discription:"This is my freelance project",
       livelink:"/",
-      gitlink:"/",
+      gitlink:"https://github.com/isham-26",
 
     },
     {
       num:1,
       title:"mark website",
       categiry:"full stack",
-      img:"project.jpg",
-      discription:"lorem bnqwsd usd qwsuw sws swsqw sws sisqws i sqwghws wsus sw suiz shqwsuq sqwuishs qws qwhs",
-      livelink:"/",
-      gitlink:"/",
+      img:"project-1.png",
+      tools:[
+        <FaReact key={9} />,
+        <FaNodeJs key={8} />,
+        <SiExpress key={7} />,
+        <TbBrandNextjs key={6} />,
+        <SiTailwindcss key={5} />,],
+      discription:"lorem bnqwsd usd qwsuw sws swsqw sws sisqws i sqwghws wsus sw suiz shqwsuq sqwuishs qws qwhs lorem hbwsxd quiwbsd qwi sdhqiw dsqiwd bqwd quwd qwud bqwdbqw dquwdbqwdvq quwdbq duqwdbqwdbqwdbquwd dqwudb qwudquwd qwudqw dquwdq dquwd  qdqud quwd qwd qudqwd ",
+      livelink:"https://marq-one.vercel.app/",
+      gitlink:"https://github.com/isham-26/marq",
+
+    },
+    {
+      num:1,
+      title:"mark website",
+      categiry:"full stack",
+      img:"project-1.png",
+      tools:[
+        <FaReact key={9} />,
+        <FaNodeJs key={8} />,
+        <SiExpress key={7} />,
+        <TbBrandNextjs key={6} />,
+        <SiTailwindcss key={5} />,],
+      discription:"lorem bnqwsd usd qwsuw sws swsqw sws sisqws i sqwghws wsus sw suiz shqwsuq sqwuishs qws qwhs lorem hbwsxd quiwbsd qwi sdhqiw dsqiwd bqwd quwd qwud bqwdbqw dquwdbqwdvq quwdbq duqwdbqwdbqwdbquwd dqwudb qwudquwd qwudqw dquwdq dquwd  qdqud quwd qwd qudqwd ",
+      livelink:"https://marq-one.vercel.app/",
+      gitlink:"https://github.com/isham-26/marq",
+
+    },
+    {
+      num:1,
+      title:"mark website",
+      categiry:"full stack",
+      img:"project-1.png",
+      tools:[
+        <FaReact key={9} />,
+        <FaNodeJs key={8} />,
+        <SiExpress key={7} />,
+        <TbBrandNextjs key={6} />,
+        <SiTailwindcss key={5} />,],
+      discription:"lorem bnqwsd usd qwsuw sws swsqw sws sisqws i sqwghws wsus sw suiz shqwsuq sqwuishs qws qwhs lorem hbwsxd quiwbsd qwi sdhqiw dsqiwd bqwd quwd qwud bqwdbqw dquwdbqwdvq quwdbq duqwdbqwdbqwdbquwd dqwudb qwudquwd qwudqw dquwdq dquwd  qdqud quwd qwd qudqwd ",
+      livelink:"https://marq-one.vercel.app/",
+      gitlink:"https://github.com/isham-26/marq",
 
     },
   ]
   return (
     <div className="flex justify-center items-center">
-      <div className="xl:w-[70%] w-full p-4 xl:p-0">
+      <div className="xl:w-[85%] flex flex-wrap justify-center gap-7 p-4 xl:p-0">
         {
            data.map((item,indx)=>{
               return (
--- components/Card.jsx
+++ components/Card.jsx
@@ -4,29 +4,47 @@ import React from "react";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 const Card = ({ item }) => {
   return (
-    <div className="flex flex-col xl:flex-row xl:gap-7 gap-4 bg-pink-50/20 rounded-md mb-3">
-      <div className="xl:w-1/2 overflow-hidden bg-white rounded-tl-md rounded-tr-md xl:rounded-tl-none xl:rounded-tr-none">
+    <div className="xl:w-[36rem] xl:gap-7 gap-4 bg-pink-50/20 rounded-md mb-3 xl:hover:scale-105 ease-in-out duration-500">
+      <div className="overflow-hidden bg-white rounded-tl-md rounded-tr-md ">
         <Image
           src={`/${item.img}`}
-          className="hover:scale-105 w-full ease-in-out duration-500 hover:opacity-80 object-contain"
+          className="h-[15rem] xl:h-[18rem] hover:scale-105 w-full ease-in-out duration-500 hover:opacity-80 object-cover"
           alt="img"
           width={300}
-          height={200}
+          height={400}
         />
       </div>
-      <div className="xl:w-1/2 flex flex-col gap-2 justify-center p-3">
-        <div className="xl:text-2xl text-xl text-white font-semibold">0{item.num}</div>
+      <div className="flex flex-col gap-2 justify-center p-5">
+        <div className="xl:text-2xl text-xl text-white font-semibold">
+          0{item.num}
+        </div>
         <div className="xl:text-4xl text-3xl text-accent">{item.title}</div>
-        <div className="xl:text-2xl text-xl text-white text-bold">{item.categiry}</div>
-        <p className="text-bold text-gray-300">{item.discription}</p>
+        <div className="xl:text-2xl text-xl text-white text-bold">
+          {item.categiry}
+        </div>
+        <p className="text-bold text-gray-300 line-clamp-5 leading-6">
+          {item.discription}
+        </p>
+        <div className="">
+          <div className="text-xl"><span className="text-accent">* </span>Using Tools</div>
+          <div className="flex gap-5 text-2xl mt-3 text-accent">
+          {item.tools.map((icon, index) => {
+            return (
+              <span className="" key={index}>
+                {icon}
+              </span>
+            );
+          })}
+          </div>
+        </div>
         <div className="flex gap-5">
-          <Link href={item.livelink}>
-            <div className="w-[2rem] h-[2rem] xl:w-[3rem] xl:h-[3rem] rounded-full bg-accent flex justify-center items-center xl:text-2xl">
+          <Link href={item.livelink} target="_balank">
+            <div className="w-[2rem] h-[2rem] xl:w-[3rem] xl:h-[3rem] rounded-full bg-pink-50/20 flex justify-center items-center xl:text-2xl hover:bg-accent text-accent hover:text-white ease-in-out duration-300">
               <BsArrowUpRight />
             </div>
           </Link>
-          <Link href={item.gitlink}>
-            <div className="w-[2rem] h-[2rem] xl:w-[3rem] xl:h-[3rem] rounded-full bg-accent flex justify-center items-center xl:text-2xl">
+          <Link href={item.gitlink} target="_balank">
+            <div className="w-[2rem] h-[2rem] xl:w-[3rem] xl:h-[3rem] rounded-full bg-pink-50/20 flex justify-center items-center xl:text-2xl hover:bg-accent text-accent hover:text-white ease-in-out duration-300">
               <BsGithub />
             </div>
           </Link>
